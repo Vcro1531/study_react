1eaec6e44141e83b3120d9a751e6b5745eed729b
KimJiMin <minmin4736@naver.com>
2022-06-13 23:40:10 +0900

    ⚡️ :: Function에서 state hook 사용 - 3

--- src/App.js
+++ src/App.js
@@ -15,16 +15,29 @@ function FuncComp(props) {
   var numberState = useState(props.initNumber);
   var number = numberState[0];
   var setNumber = numberState[1];
+
+  var dateState = useState(new Date().toString());
+  var _date = dateState[0];
+  var setDate = dateState[1];
+
   console.log("numberState", numberState);
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number : {number}</p>
+      <p>Date : {_date}</p>
       <input
         type="button"
         value="random"
         onClick={function () {
           setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
         }}
       ></input>
     </div>
@@ -41,11 +54,19 @@ class ClassComp extends React.Component {
       <div className="container">
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={function () {
             this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
           }.bind(this)}
         ></input>
       </div>
