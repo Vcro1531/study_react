0842da9ef3409390b47fa763a0002d10b68019ee
KimJiMin <minmin4736@naver.com>
2022-06-27 14:11:24 +0900

    ⚡️ :: skipping Effect test
    잘 모르겠음

--- src/App.js
+++ src/App.js
@@ -26,10 +26,51 @@ function FuncComp(props) {
 
   useEffect(function () {
     console.log(
-      "%cfunc => useEffect (componentDidMount & componentDidUpdate)" + ++funcId,
+      "%cfunc => useEffect number (componentDidMount & componentDidUpdate)" + ++funcId,
       funcStyle
     );
-  });
+    document.title = number;
+    return function () {
+      console.log(
+        "%cfunc => useEffect return (componentDidMount & componentDidUpdate) " + ++funcId,
+        funcStyle
+      );
+    };
+  }, []);
+
+  useEffect(
+    function () {
+      console.log(
+        "%cfunc => useEffect number (componentDidMount & componentDidUpdate)" + ++funcId,
+        funcStyle
+      );
+      document.title = number;
+      return function () {
+        console.log(
+          "%cfunc => useEffect number return (componentDidMount & componentDidUpdate) " + ++funcId,
+          funcStyle
+        );
+      };
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      console.log(
+        "%cfunc => useEffect date (componentDidMount & componentDidUpdate)" + ++funcId,
+        funcStyle
+      );
+      document.title = _date;
+      return function () {
+        console.log(
+          "%cfunc => useEffect _date return (componentDidMount & componentDidUpdate) " + ++funcId,
+          funcStyle
+        );
+      };
+    },
+    [_date]
+  );
 
   console.log("%cfunc => render" + ++funcId, funcStyle);
   return (
