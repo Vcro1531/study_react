6a80ead3f25c8ee987af30531fd16410cad089be
KimJiMin <minmin4736@naver.com>
2022-07-04 22:23:08 +0900

    ⚡️ :: CSS

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  const name = "리액트";
-  return <>{name === "리액트" ? <h1>리액트입니다</h1> : <h2>리액트가 아닙니다</h2>}</>;
+  const name = "리액트입니다";
+  return <div className="react">{name}</div>;
 }
 
 export default App;
